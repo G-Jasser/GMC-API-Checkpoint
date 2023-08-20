8bceaa97232f1c4b151e4c4361c9bea5aee5a662
Jasser Gharb <123377848+G-Jasser@users.noreply.github.com>
2023-08-20 21:56:58 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <p className="read-the-docs">
+          Nice
+        </p>  
+      </div>
+    </>
+  )
+}
+
+export default App
